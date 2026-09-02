426948101c0716d145c7c71d2d03b353d2f51559
Steve Maughan <stevemaughan@live.com>
2026-09-02 03:31:06 -0400

    pgo script: clean profile dir

--- source/build_pgo.sh
+++ source/build_pgo.sh
@@ -3,9 +3,9 @@
 cd "$(dirname "$0")"
 OUT=${1:-build/engine_pgo.exe}
 mkdir -p build/pgo
-rm -f build/pgo/*.gcda
+rm -rf build/pgo; mkdir -p build/pgo
 SRCS="uci.cpp search.cpp bitboard.cpp position.cpp movegen.cpp eval.cpp datagen.cpp"
-FLAGS="-O3 -flto -static -march=x86-64-v3 -mtune=znver4 -std=c++17 -DNDEBUG -fno-exceptions -fno-rtti -Wall -Wno-unused-result"
+FLAGS="-O3 -flto -static -march=x86-64-v3 -mtune=znver4 -std=c++17 -DNDEBUG -DNO_TUNE_OPTIONS -fno-exceptions -fno-rtti -Wall -Wno-unused-result"
 g++ $FLAGS -fprofile-generate -fprofile-dir=build/pgo -o build/pgo/gen.exe $SRCS || exit 1
 ./build/pgo/gen.exe bench 13 > /dev/null
 g++ $FLAGS -fprofile-use -fprofile-dir=build/pgo -fprofile-correction -Wno-missing-profile -o "$OUT" $SRCS || exit 1
